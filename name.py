86f99ad9fbb9c4a461ed2e49c8aaf2f4de9111bb
meeia <102783643+cnmeeia@users.noreply.github.com>
2022-07-05 10:57:41 +0800

    Update name.py

--- name.py
+++ name.py
@@ -5,7 +5,7 @@ files = os.listdir('.')
 for filename in files:
 	portion = os.path.splitext(filename)
 	# 如果后缀是.dat
-	if portion[1] == ".jpeg":  
+	if portion[1] == ".jpeg|webm":  
 		# 重新组合文件名和后缀名
 		newname = portion[0] + ".png"   
 		os.rename(filename,newname)
